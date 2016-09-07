bb4cbacac46d4f8b844e292b34c449a2d8437656
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2016-09-07 09:33:10 +0200

    Separate launcher and main class

--- src/main/java/org/netbeans/html/demo/node4j/Launcher.java
+++ src/main/java/org/netbeans/html/demo/node4j/Launcher.java
@@ -3,15 +3,11 @@ package org.netbeans.html.demo.node4j;
 import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.api.interop.java.JavaInterop;
 import java.io.Closeable;
+import java.io.IOException;
 import java.util.concurrent.Executor;
 import net.java.html.boot.truffle.TrufflePresenters;
-import net.java.html.lib.Function;
 import net.java.html.lib.Modules;
 import net.java.html.lib.Objs;
-import net.java.html.lib.node.http.Exports;
-import net.java.html.lib.node.http.IncomingMessage;
-import net.java.html.lib.node.http.ServerResponse;
-import net.java.html.lib.node.http.Server;
 import org.netbeans.html.boot.spi.Fn;
 
 public final class Launcher extends Modules.Provider implements Executor {
@@ -21,28 +17,21 @@ public final class Launcher extends Modules.Provider implements Executor {
         this.require = JavaInterop.asJavaFunction(CommonJS.class, require);
     }
 
-    public void main() {
-        Server server = Exports.createServer((IncomingMessage request, ServerResponse response) -> {
-            System.err.println("request for " + request.url());
-            response.end("It works! Hit " + request.url());
-            return null;
-        });
-        server.listen(8080, Function.newFunction((Function.A0<Void>) () -> {
-            System.err.println("Server listening on port 8080");
-            return null;
-        }));
-    }
-
     public void run() throws Exception {
         Fn.Presenter p = TrufflePresenters.create(this);
         try (Closeable c = Fn.activate(p)) {
-            main();
+            Main.main();
         }
     }
 
     @Override
     public void execute(Runnable command) {
-        command.run();
+        Fn.Presenter p = TrufflePresenters.create(this);
+        try (Closeable c = Fn.activate(p)) {
+            command.run();
+        } catch (IOException ex) {
+            throw new IllegalStateException(ex);
+        }
     }
 
     @Override
---
+++ src/main/java/org/netbeans/html/demo/node4j/Main.java
@@ -0,0 +1,23 @@
+package org.netbeans.html.demo.node4j;
+
+import net.java.html.lib.Function;
+import net.java.html.lib.node.http.Exports;
+import net.java.html.lib.node.http.IncomingMessage;
+import net.java.html.lib.node.http.ServerResponse;
+import net.java.html.lib.node.http.Server;
+
+public final class Main  {
+    public static void main(String... args) {
+        Server server = Exports.createServer((IncomingMessage request, ServerResponse response) -> {
+            System.err.println("request for " + request.url());
+            response.end("It works! Hit " + request.url());
+            return null;
+        });
+        server.listen(8080, Function.newFunction((Function.A0<Void>) () -> {
+            System.err.println("Server listening on port 8080");
+            return null;
+        }));
+    }
+
+
+}
