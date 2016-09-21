0e2abe2d97a1123d31c1089ee8c5de2bf1d334ab
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2016-09-21 20:16:02 +0200

    Static import of createServer

--- src/main/java/org/netbeans/demo/node4j/Main.java
+++ src/main/java/org/netbeans/demo/node4j/Main.java
@@ -1,14 +1,14 @@
 package org.netbeans.demo.node4j;
 
 import net.java.html.lib.Function;
-import net.java.html.lib.node.http.Exports;
+import static net.java.html.lib.node.http.Exports.createServer;
 import net.java.html.lib.node.http.IncomingMessage;
 import net.java.html.lib.node.http.ServerResponse;
 import net.java.html.lib.node.http.Server;
 
 public final class Main  {
     public static void main(String... args) {
-        Server server = Exports.createServer((IncomingMessage request, ServerResponse response) -> {
+        Server server = createServer((IncomingMessage request, ServerResponse response) -> {
             System.err.println("request for " + request.url());
             response.end("It works! Hit " + request.url());
             return null;
