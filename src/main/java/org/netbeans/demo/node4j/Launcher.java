95f9466a0c6302e75f1ec1a1c190c1625c67659d
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2016-11-09 14:02:35 +0100

    Using the Executor provided by the Graal node.js implementation

--- src/main/java/org/netbeans/demo/node4j/Launcher.java
+++ src/main/java/org/netbeans/demo/node4j/Launcher.java
@@ -1,67 +1,38 @@
 package org.netbeans.demo.node4j;
 
+import com.oracle.truffle.api.impl.TruffleLocator;
 import java.io.Closeable;
-import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
+import java.lang.reflect.Method;
 import java.util.concurrent.Executor;
 import net.java.html.boot.truffle.TrufflePresenters;
-import net.java.html.lib.Array;
 import net.java.html.lib.Function;
 import net.java.html.lib.Modules;
 import net.java.html.lib.Objs;
-import net.java.html.lib.node.Exports;
 import org.netbeans.html.boot.spi.Fn;
 
 public final class Launcher extends Modules.Provider implements Executor {
     private final Function require;
     private final Objs global;
-    private final List<Runnable> queue = new LinkedList<>();
+    private final Executor delegate;
     private final Fn.Presenter presenter;
 
-    public Launcher(Object require, Object global) {
+    public Launcher(Object require, Object global) throws Exception {
         this.require = Function.$as(require);
         this.global = Objs.$as(global);
         this.presenter = TrufflePresenters.create(this);
+        this.delegate = findExecutor();
         System.getProperties().put("org.netbeans.lib.jshell.agent.AgentWorker.executor", this);
     }
 
     public void initialize() throws Exception {
         try (Closeable c = Fn.activate(presenter)) {
             Main.main();
-            Exports.setInterval((Array<Object> p1) -> {
-                clearQueue();
-                return null;
-            }, 100);
         }
     }
 
     @Override
-    public synchronized void execute(Runnable command) {
-        queue.add(command);
-    }
-
-    private synchronized Runnable[] emptyQueue() {
-        if (queue.isEmpty()) {
-            return null;
-        }
-        Runnable[] arr = queue.toArray(new Runnable[0]);
-        queue.clear();
-        return arr;
-    }
-
-    private void clearQueue() {
-        Runnable[] pending = emptyQueue();
-        if (pending == null) {
-            return;
-        }
-        try (Closeable c = Fn.activate(presenter)) {
-            for (Runnable r : pending) {
-                r.run();
-            }
-        } catch (IOException ex) {
-            throw new IllegalStateException(ex);
-        }
+    public void execute(Runnable command) {
+        delegate.execute(command);
     }
 
     @Override
@@ -71,5 +42,13 @@ public final class Launcher extends Modules.Provider implements Executor {
         }
         Object module = require.apply(null, id);
         return Objs.$as(module);
+    }
+
+    private static Executor findExecutor() throws Exception {
+        Method loadClass = TruffleLocator.class.getDeclaredMethod("loadClass", String.class);
+        loadClass.setAccessible(true);
+        Class<?> loopClass = (Class<?>) loadClass.invoke(null, "com.oracle.truffle.trufflenode.EventLoop");
+        Method get = loopClass.getMethod("get");
+        return (Executor) get.invoke(null);
     }
 }
